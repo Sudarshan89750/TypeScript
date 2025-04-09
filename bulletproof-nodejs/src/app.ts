b74c565602b4f65d7bda6d6cbf1e7afe052334a3
Sudarshan89750 <sudarshanpawar6425@gmail.com>
2025-04-09 11:33:33 +0530

    Refactor app.ts for consistent import style and add order route integration

--- bulletproof-nodejs/src/app.ts
+++ bulletproof-nodejs/src/app.ts
@@ -1,10 +1,9 @@
-import 'reflect-metadata'; // We need this in order to use @Decorators
+import "reflect-metadata"; // We need this in order to use @Decorators
 
-import config from './config';
-
-import express from 'express';
-
-import Logger from './loaders/logger';
+import config from "./config";
+import express from "express";
+import order from "./api/routes/order.route";
+import Logger from "./loaders/logger";
 
 async function startServer() {
   const app = express();
@@ -15,8 +14,11 @@ async function startServer() {
    * Well, at least in node 10 without babel and at the time of writing
    * So we are using good old require.
    **/
-  await require('./loaders').default({ expressApp: app });
-
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  await require("./loaders").default({ expressApp: app });
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use("/orders", order);
   app
     .listen(config.port, () => {
       Logger.info(`
@@ -25,7 +27,7 @@ async function startServer() {
       ################################################
     `);
     })
-    .on('error', err => {
+    .on("error", (err) => {
       Logger.error(err);
       process.exit(1);
     });
