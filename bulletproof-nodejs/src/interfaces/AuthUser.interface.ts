c1f8d0a9b7191718ddcca13376c9ed37ec8aa3d8
Sudarshan89750 <sudarshanpawar6425@gmail.com>
2025-04-10 11:53:53 +0530

    AuthUser Dto added

--- bulletproof-nodejs/src/interfaces/AuthUser.interface.ts
+++ bulletproof-nodejs/src/interfaces/AuthUser.interface.ts
@@ -2,3 +2,17 @@ export interface AuthUser {
   name: string;
   password: string;
 }
+export class AuthUserDto {
+  name: string;
+  password: string;
+  constructor(name: string, password: string) {
+    if (typeof name !== "string") {
+      throw new Error("Name must be a string");
+    }
+    if (typeof password !== "string") {
+      throw new Error("Password must be a string");
+    }
+    this.name = name;
+    this.password = password;
+  }
+}
